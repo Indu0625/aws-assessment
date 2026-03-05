6f89eca5f63c6ef629b9213b861cb9b71f163842
lll <lll@gmail.com>
2026-03-06 03:55:32 +0530

    Fix terraform formatting

--- modules/regional_stack/main.tf
+++ modules/regional_stack/main.tf
@@ -49,8 +49,8 @@ resource "aws_iam_role_policy" "lambda_dynamo_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
-      Action = ["dynamodb:PutItem"]
+      Effect   = "Allow"
+      Action   = ["dynamodb:PutItem"]
       Resource = aws_dynamodb_table.greeting_logs.arn
     }]
   })
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "cognito_user_pool_id" {
-  type = string
-  default="dummy-user-pool"
+  type    = string
+  default = "dummy-user-pool"
 }
 
 variable "cognito_client_id" {
-  type = string
-  default="dummy-user-pool"
+  type    = string
+  default = "dummy-user-pool"
 }
